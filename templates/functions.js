270dae9eb49e96933f6524fbdeb45a872108adc5
javohirhalimov <88170054+javohirhalimov@users.noreply.github.com>
2022-02-12 23:04:09 -0500

    Add files via upload

---
+++ templates/functions.js
@@ -0,0 +1,116 @@
+
+
+
+  var count = document.querySelector('.numItem')
+  var btn1 = document.querySelectorAll('a.add');
+  var infoButton = document.querySelectorAll('container.link')
+  buttonLength = btn1.length;
+  for(let i=0; i<buttonLength; i++){
+    btn1[i].addEventListener('click', function(){
+    alert("Item was added!")   
+    })
+  }
+
+
+  function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topNav";
+  }
+  }
+
+  let toTopDiv = document.querySelector('.toTop');
+  toTopDiv.style.display = 'none';
+  window.addEventListener('scroll', function(){
+    let pxWin = window.scrollY;
+    if (pxWin>1400) {
+      toTopDiv.style.display = 'block';
+    }
+    else{
+      toTopDiv.style.display = 'none';
+    }
+  })
+
+  function item1(){
+    // infoButton.innerHTML = localStorage.getItem('btn_Name')
+    let btnText = 'Added to Cart'
+    localStorage.setItem('btn_Name',btnText)
+    count+=1;
+    localStorage.setItem('countNum',count)
+    infoButton.style.backgroundColor="green";
+
+    let productName = 'Le Labo SANTAL 33'
+    let productPrice = 289.00
+    localStorage.setItem('product_Name',productName);
+    localStorage.setItem('product_Price',productPrice);
+    return false;
+  }
+
+
+  function item2(){
+    // infoButton.innerHTML = localStorage.getItem('btn_Name')
+    let btnText = 'Added to Cart'
+    localStorage.setItem('btn_Name',btnText)
+    count+=1;
+    localStorage.setItem('countNum',count)
+    let productName = 'Chanel BLEU DE CHANEL'
+    let productPrice = 132.00
+    localStorage.setItem('product_Name',productName);
+    localStorage.setItem('product_Price',productPrice);
+    return false;
+  }
+
+  function item3(){
+    // infoButton.innerHTML = localStorage.getItem('btn_Name')
+    let btnText = 'Added to Cart'
+    localStorage.setItem('btn_Name',btnText)
+    count+=1;
+    localStorage.setItem('countNum',count)
+    let productName = 'Byredo BLACK SAFFRON'
+    let productPrice = 270.00
+    localStorage.setItem('product_Name',productName);
+    localStorage.setItem('product_Price',productPrice);
+    return false;
+  }
+
+  function item4(){
+    let infoButton = document.querySelector('.link');
+    // infoButton.innerHTML = localStorage.getItem('btn_Name')
+    let btnText = 'Added to Cart'
+    localStorage.setItem('btn_Name',btnText)
+    count+=1;
+    localStorage.setItem('countNum',count)
+    let productName = 'Hugo Boss BOSS BOTTLED'
+    let productPrice = 75.00
+    localStorage.setItem('product_Name',productName);
+    localStorage.setItem('product_Price',productPrice);
+    return false;
+  }
+
+  function item5(){
+      // infoButton.innerHTML = localStorage.getItem('btn_Name')
+    let btnText = 'Added to Cart'
+    localStorage.setItem('btn_Name',btnText)
+    count+=1;
+    localStorage.setItem('countNum',count)
+    let productName = 'Gucci GUILTY EDT'
+    let productPrice = 119.00
+    localStorage.setItem('product_Name',productName);
+    localStorage.setItem('product_Price',productPrice);
+    return false;
+  }
+
+  function item6(){
+    // infoButton.innerHTML = localStorage.getItem('btn_Name')
+    let btnText = 'Added to Cart'
+    localStorage.setItem('btn_Name',btnText)
+    count+=1;
+    localStorage.setItem('countNum',count)
+    let productName = 'English Laundry NOIR CASTLE'
+    let productPrice = 95.00
+    localStorage.setItem('product_Name',productName);
+    localStorage.setItem('product_Price',productPrice);
+    return false;
+  }
